c149d4235e94dd5a4a1281d544698dc7145ac76d
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-11-03 10:01:35 +0100

    Update urls.py

--- moodlestudent/profiles/urls.py
+++ moodlestudent/profiles/urls.py
@@ -4,5 +4,5 @@ app_name="profiles"
 urlpatterns = [
     path('', views.list_course, name='index'),
     path('view_course/', views.view_course, name='view_course'),
-    path('add/<int:course_id>', views.add_course, name='add_course'),
+    path('add_course/<int:course_id>', views.add_course, name='add_course'),
 ]
