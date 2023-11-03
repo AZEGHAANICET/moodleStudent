1717da3b933750db5fd3d064cc275f2092f2edf4
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-11-03 09:21:47 +0100

    Update urls.py

--- moodlestudent/profiles/urls.py
+++ moodlestudent/profiles/urls.py
@@ -1,1 +1,8 @@
-
+from django.urls import path, include
+from . import views
+app_name="profiles"
+urlpatterns = [
+    path('', views.list_course, name='index'),
+    path('view_course/', views.view_course, name='view_course'),
+    path('add/<int:course_id>', views.add_course, name='add_course'),
+]
