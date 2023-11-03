082c1b8d9325fbc099b75d27067b2c01c87f1828
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-11-03 09:14:50 +0100

    Update models.py

--- moodlestudent/profiles/models.py
+++ moodlestudent/profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from course.models import Course
 # Create your models here.
 class Profile(models.Model):
     COMPUTER_SCIENCE='Computer Science'
@@ -15,4 +16,5 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
     image = models.ImageField(max_length=255, default=None, upload_to='images/users/%Y/%m/%d', blank=True)
     filiere = models.CharField(max_length=20, default= COMPUTER_SCIENCE,choices =CHOICE_TYPE_OF_COURSE) 
+    courses = models.ManyToManyField(Course)
     
