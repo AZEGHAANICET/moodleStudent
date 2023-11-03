4aafb5bf1de52749d70dd33a604c100f2e0f93c7
azegha <loicanicetazegha@gmail.com>
2023-11-03 07:55:17 +0100

    Ajout de la fonctionnalité inscription , creation de cours

--- moodlestudent/course/models.py
+++ moodlestudent/course/models.py
@@ -1,3 +1,61 @@
 from django.db import models
-
+import os 
+from django.core.exceptions import ValidationError
+from datetime import datetime
+from PIL import Image
 # Create your models here.
+class Course(models.Model):
+    AUTOMNE='Automne'
+    PRINTEMPS='Printemps'
+    COMPUTER_SCIENCE='Computer Science'
+    A2I='A2I'
+    MECHANICAL='Mechanical'
+    GI = 'GI'
+    CHOICE_TYPE_OF_COURSE=[
+        (COMPUTER_SCIENCE, "Computer Science"),
+        (A2I, "A2I"),
+        (MECHANICAL, "Mechanical"),
+        (GI, "GI")
+    ]
+    CHOICES_TIME = [(AUTOMNE, 'Automne'),
+    (PRINTEMPS, 'Printemps'),]
+    def get_file_upload_path(instance, filename):
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        return os.path.join("images/Files", current_time, instance.typ, filename)
+    def get_image_upload_path(instance, filename):
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        return os.path.join("images/courses", current_time, instance.typ, filename)
+
+    name = models.CharField(max_length=200)
+    typ = models.CharField(max_length =20, choices=CHOICE_TYPE_OF_COURSE, default=COMPUTER_SCIENCE)
+    intervenant = models.CharField(max_length=200, default=None)
+    creation_date = models.DateField(auto_now_add=True)
+    file = models.FileField(max_length=200, upload_to=get_file_upload_path)
+    time = models.CharField(max_length=10, choices =CHOICES_TIME, default=AUTOMNE)
+    description = models.TextField(default=None)
+    image = models.ImageField(default=None, blank=True, upload_to=get_image_upload_path)
+
+
+    def deux_premieres_phrases(self):
+        phrases = self.description.split('.')
+        deux_premieres_phrases = '.'.join(phrases[:2])
+        return deux_premieres_phrases
+
+    def set_intervenant(self, value):
+        self.intervenant = value
+    def __str__(self):
+        return self.name
+    def clean(self):
+        print("clean pro")
+        super().clean()
+        if self.file:
+            max_size = 10 * 1024 * 1024
+            if self.file.size > max_size:
+                raise ValidationError("La taille du fichier ne doit pas dépasser 10 Mo.")
+ 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        new_size = (300, 200) 
+        img.thumbnail(new_size)
+        img.save(self.image.path)
--- moodlestudent/course/views.py
+++ moodlestudent/course/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden, HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Course
+from .forms import CreateCourse
+from django.core.paginator import Paginator, EmptyPage,  PageNotAnInteger
 
 # Create your views here.
+
+@login_required
+def index(request):
+    print(request.user.profile.filiere)
+    course_name = request.GET.get('course_name')
+    courses = Course.objects.filter(typ__icontains=request.user.profile.filiere)
+    list_courses = courses
+
+    if course_name and course_name != '':
+        courses = courses.filter(name__icontains=course_name)
+
+    paginator = Paginator(courses, 3)
+    page = request.GET.get('page', 1) 
+
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        courses = paginator.page(1)
+    except EmptyPage:
+        courses = paginator.page(paginator.num_pages)
+
+    return render(request, 'course/index.html', {'courses': courses, 'list_courses': list_courses})
+
+
+
+
+@login_required
+def create_course(request):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden('Impossoble de créer des cours vous n\' êtes pas un professeur')
+    else:
+        if request.method == 'POST':
+            form = CreateCourse(data=request.POST, files = request.FILES)
+            if form.is_valid():
+                new_course = form.save(commit=False)
+                new_course.set_intervenant(request.user.username)
+                new_course.save()
+                return HttpResponse('Course create with success')
+
+        else:
+            form = CreateCourse()
+        return render(request, "course/create_course.html", {'form': form})
+
+
+def view_course(request, course_id):
+    course_object = Course.objects.get(id=course_id)
+    return render(request, "course/view_course.html", {'course_object': course_object })
