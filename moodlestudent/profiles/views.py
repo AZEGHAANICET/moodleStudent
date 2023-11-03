504001a02ff035b103ef120ef869a56026338bec
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-11-03 09:40:18 +0100

    Update views.py

--- moodlestudent/profiles/views.py
+++ moodlestudent/profiles/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
+from course.models import Course
+from django.http import JsonResponse
 # Create your views here.
 
+def list_course(request):
+  profiles = get_object_or_404(Profile,pk=request.user.profile.id)
+  return render(request, 'profile/list_course.html', {'profiles':profiles})
 
+
+def add_course(request, course_id):
+  try:
+    course = Course.objects.get(pk= course_id)
+    request.user.profile.courses.add(course)
+    return JsonResponse({'success':True})
+  except Exception as e:
+    return JsonResponse({'success':False, 'error':str(e)})
+
+
+
+  
+  
+  
